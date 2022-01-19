4049f12c5dea9f94065a028e57b89568cbf0ea47
Krishna-Kolukuluri <krish.kolukuluri@gmail.com>
2022-01-18 22:25:08 -0500

    Create TwoElementSumToTargetWayfair.java

---
+++ src/main/java/CommonProblems/TwoElementSumToTargetWayfair.java
@@ -0,0 +1,7 @@
+package CommonProblems;
+/*
+* Given an array of weights and a target weight find if the array contains two values that are equal to the target weight.
+eg: [1 2 5 6 9], Target 10 : true
+* */
+public class TwoElementSumToTargetWayfair {
+}
