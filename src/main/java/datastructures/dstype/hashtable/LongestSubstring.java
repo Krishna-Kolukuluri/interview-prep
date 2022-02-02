7b4ad8d771bc255ef982a8453035ce81393cfddb
Krishna-Kolukuluri <krish.kolukuluri@gmail.com>
2022-02-01 23:20:47 -0500

    updates

--- src/main/java/datastructures/dstype/hashtable/LongestSubstring.java
+++ src/main/java/datastructures/dstype/hashtable/LongestSubstring.java
@@ -37,4 +37,34 @@ public class LongestSubstring {
         }
         return curMax;
     }
+
+    /*
+    *
+    Approach 3: Sliding Window Optimized
+    The above solution requires at most 2n steps. In fact, it could be optimized to require only n steps. Instead of using a
+    set to tell if a character exists or not, we could define a mapping of the characters to its index. Then we can skip the
+    characters immediately when we found a repeated character.
+
+    The reason is that if s[j] have a duplicate in the range [i,j) with index j we don't need to increase i little by little.
+    We can skip all the elements in the range [i,j] and let i to be j+ 1 directly.
+    * */
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+    /*
+    Complexity Analysis:
+    Time complexity : O(n). Index jj will iterate nn times.
+    Space complexity (HashMap) : O(min(m,n)). Same as the previous approach.
+    Space complexity (Table): O(m). mm is the size of the charset.
+    * */
 }
