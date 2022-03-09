2d20efca4f6420ccde59a08cc28658759c900522
KrishnaKolukuluri <krish.kolukuluri@gmail.com>
2022-03-09 01:08:24 -0500

    Behavioural scenarios

--- src/main/java/patterns/recursion/dfs/DiameterOfBinaryTree.java
+++ src/main/java/patterns/recursion/dfs/DiameterOfBinaryTree.java
@@ -1,5 +1,7 @@
 package patterns.recursion.dfs;
 
+import recursion.TreeNode;
+
 /*
 * https://leetcode.com/problems/diameter-of-binary-tree/
 * diameter of binary tree or length of the longest path between any two nodes in a tree
@@ -9,4 +11,33 @@ The diameter of a binary tree is the length of the longest path between any two 
 The length of a path between two nodes is represented by the number of edges between them.
 * */
 public class DiameterOfBinaryTree {
+    private int diameter;
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameter = 0;
+        longestPath(root);
+        return diameter;
+    }
+    private int longestPath(TreeNode node){
+        if(node == null) return 0;
+        // recursively find the longest path in
+        // both left child and right child
+        int leftPath = longestPath(node.left);
+        int rightPath = longestPath(node.right);
+
+        // update the diameter if left_path plus right_path is larger
+        diameter = Math.max(diameter, leftPath + rightPath);
+
+        // return the longest one between left_path and right_path;
+        // remember to add 1 for the path connecting the node and its parent
+        return Math.max(leftPath, rightPath) + 1;
+    }
 }
+/*
+* Complexity Analysis:
+    Let N be the number of nodes in the tree.
+    Time complexity: O(N). This is because in our recursion function longestPath, we only enter and exit from each node once.
+    We know this because each node is entered from its parent, and in a tree, nodes only have one parent.
+
+    Space complexity: O(N). The space complexity depends on the size of our implicit call stack during our DFS, which relates
+    to the height of the tree. In the worst case, the tree is skewed so the height of the tree is O(N). If the tree is balanced, it'd be O(logN).
+* */
